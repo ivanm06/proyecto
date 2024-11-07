26118534885ada6b182f3f016bdde32a458adc3a
Ivan Martin <ivan.mart.rivig@gmail.com>
2024-11-07 09:09:23 +0100

    commit tres

--- src/App.java
+++ src/App.java
@@ -3,7 +3,5 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
     }
 }
