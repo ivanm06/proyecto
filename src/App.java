24b5d10c475c3b21ce011d5c2ea25ed727f179b4
Ivan Martin <ivan.mart.rivig@gmail.com>
2024-11-07 09:32:47 +0100

    commit ivan

--- src/App.java
+++ src/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
     }
 }
